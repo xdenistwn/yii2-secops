e9c615313a61c321d3bd1a7cd595713bb2f677f7
xdenistwn <ofc.denisetiawan@gmail.com>
2026-09-02 12:52:56 +0700

    add some code smell

---
+++ views/report/feedback.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string|null $name */
+
+$this->title = 'Report Feedback';
+?>
+<div class="report-feedback">
+    <h1><?= $this->title ?></h1>
+
+    <p>Hi <?= $name ?>, please tell us what you think about this report.</p>
+
+    <form method="post">
+        <textarea name="comment" rows="4" cols="50"></textarea>
+        <br>
+        <button type="submit">Send feedback</button>
+    </form>
+</div>
